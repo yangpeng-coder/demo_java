b42a26b31f5499f3881a0656e1de735a3944d48c
yangpeng-coder <yp.asolu@gmail.com>
2021-01-29 17:59:02 +0900

    classを改善

--- src/h_you/demo/City.java
+++ src/h_you/demo/City.java
@@ -1,16 +1,44 @@
 package h_you.demo;
 
 public class City {
-    public String name;
-    public Double latitude;
-    public Double longitude;
+    private String name;
+    private Double latitude;
+    private Double longitude;
+
+    // 属性を取得
+    public String getName() {
+        return this.name;
+    }
+
+    public Double getLatitude() {
+        return this.latitude;
+    }
+
+    public Double getLongitude() {
+        return this.longitude;
+    }
+
+    // 構造方法
+    public City(String name, Double latitude, Double longitude) {
+        if (name == "shanghai") {
+            throw new IllegalArgumentException(name + " is not support.");
+        } else if(name == null ||name.isBlank()) {
+            this.name = defaultName();
+        } else {
+            this.name = name;
+        }
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+
+    private String defaultName() {
+        return "beijing";
+    }
 
     public static void main(String[] args) {
-        City ct = new City();
-        ct.name = "beijing";
-        ct.latitude = 39.903;
-        ct.longitude = 116.401;
-        System.out.println(ct.name);
-        System.out.println("location: " + ct.latitude + ", " + ct.longitude);
+        City ct = new City("", 39.903, 116.401);
+        System.out.println(ct.getName());
+        System.out.println("location: " + ct.getLatitude() + ", " + ct.getLongitude());
     }
 }
